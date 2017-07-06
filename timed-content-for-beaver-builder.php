5f15f28ebfb0c7ac618556686782dbdf07b6717a
Rahul <rahulw@bsf.io>
2017-07-06 10:27:47 +0530

    initial commit

---
+++ timed-content-for-beaver-builder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Timed Content for Beaver Builder
+ * Plugin URI: https://www.brainstormforce.com/
+ * Description: Timed Content for Beaver Builder is a custom modules for Beaver Builder to create module which expire after set time.
+ * Version: 1.0.0
+ * Author: Beaver Builder Addon
+ * Author URI: https://www.ultimatebeaver.com/
+ * Text Domain: timed-content-for-beaver-builder
+ *
+ * @package timed-content-for-beaver-builder
+ */
+?>
