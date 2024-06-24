bfa7f5aac61ef8971ec5f4abf4b5a9774647bfa6
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-06-24 08:37:04 +0530

    Escaped attributes

--- timed-content-module/includes/frontend.php
+++ timed-content-module/includes/frontend.php
@@ -9,11 +9,11 @@ $display = $module->is_expired( $settings );
 $is_start = $module->is_start( $settings );
 if ( $display ) {
 	if ( isset( $settings->content_type ) && 'content' == $settings->content_type ) {
-		echo '<div class="timed-content-wrapper">' . Timed_Content_Helper::get_timed_content( $settings ) . '</div>';
+		echo '<div class="timed-content-wrapper">' . Timed_Content_Helper::get_timed_content( $settings) . '</div>';
 	} else {
 		echo Timed_Content_Helper::get_timed_content( $settings );
 	}
 } elseif ( isset( $settings->expire_content_action ) && 'msg' == $settings->expire_content_action && $is_start ) {
-	echo '<' . $settings->timed_tag_selection . ' class="timed-content-message">' . $settings->expire_message . '</' . $settings->timed_tag_selection . '>';
+	echo '<' . esc_attr($settings->timed_tag_selection) . ' class="timed-content-message">' . wp_kses_post( $settings->expire_message ). '</' . esc_attr($settings->timed_tag_selection) . '>';
 }
 
